0b3fb2c6a063b53955b7d10ee8b5c8ec4cc8d30c
demola234 <kolawoleoluwasegun567@gmail.com>
2025-05-05 10:19:42 +0100

    feat: Adding Support File for project development

---
+++ Realio/Core/Services/ImageManager/ImageManager.swift
@@ -0,0 +1,7 @@
+//
+//  ImageManager.swift.swift
+//  Realio
+//
+//  Created by Ademola Kolawole on 05/05/2025.
+//
+
---
+++ Realio/Core/Utilities/NetworkError.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkError.swift
+//  Realio
+//
+//  Created by Ademola Kolawole on 05/05/2025.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case badURL
+    case requestFailed(Error)
+    case invalidResponse
+    case invalidData
+    case statusCode(Int)
+    case fileNotFound
+
+    var errorDescription: String? {
+        switch self {
+        case .badURL:
+            return "The URL is invalid."
+        case .requestFailed(let error):
+            return "The request failed with error: \(error.localizedDescription)"
+        case .invalidResponse:
+            return "The response from the server was invalid."
+        case .invalidData:
+            return "The data received from the server was invalid."
+        case .statusCode(let code):
+            return "The server responded with a status code: \(code)"
+        case .fileNotFound:
+            return "The file could not be found."
+        }
+    }
+}
